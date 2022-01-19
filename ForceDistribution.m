701789515b43dedfec7a546e047f76f5e57ef4fb
Ripunjoy Kalita <me21m065@smail.iitm.ac.in>
2022-01-19 20:39:18 +0530

    ForceDistribution.m

--- ForceDistribution.m
+++ ForceDistribution.m
@@ -10,13 +10,13 @@ mg=0;
 Mg=1200;
 F_load=mg+Mg;
 Fs=1500;
-Sections=50;
-
+Sections=150;
+Angle_N=100;
 %% Length Definition
 
 Lac=.5905;
 Lad=.493;
-Lcd=.15207;
+Lcdx=.15207;
 Lgh=.15207;
 Lcg=.880;
 Ldh=.880;
@@ -24,17 +24,19 @@ Lck=.665;
 Lce=.60978;
 Lgj=.30093;
 delta=acosd(Lac/Lce);
+Lcdy=Lac-Lad;
 
 
-%%
+%% MISCELLANEOUS
 
-theta=linspace(70,140,100);
+Strength=540; %% For mild steel
+FoS=5; %% Assumed
+Expected_Strength=Strength/FoS;
+theta=linspace(70,140,Angle_N);
 theta_2=(theta-90);
 temp=theta-delta;
 L=sqrt(Lce^2+Lck^2-2*Lce*Lck*cosd(temp));
 phi=asind(Lce.*sind(temp)./L);
-% EK=sqrt(Lck^2+Lce^2-2*Lck*Lce*cosd(temp));
-% psi=asind(EK/(Lce*sind(temp)));
 
 %% Forces
 
@@ -46,7 +48,7 @@ Fhx=(M_load./(Lgh.*sind(theta-Subtend)));
 Fgx=+Fhx-F_load*cosd(180-theta);
 Fgy=F_load*sind(180-theta);
 Fdx=Fhx;
-Fcx=-Fgx-Fx_spring;
+Fcx=Fgx+Fx_spring;
 Fcy=Fgy-Fy_spring;
 
 %% Force and moment distribution
@@ -86,6 +88,7 @@ for i=1:Sections
     l=l+dL;
 end
 
+
 F_shear_CG=flip(F_shear_CG);
 M_shear_CG=flip(M_shear_CG);
 F_axial_CG=flip(F_axial_CG);
@@ -117,10 +120,24 @@ surf(theta,l_array,M_shear_CG);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[Section,Ixx,SecArea,X,Y,Tx,Ty]=Data(Designation);
-for i=1:100
+[Designation_temp,Section,Ixx,SecArea,X,Y,Tx,Ty]=Data(Designation,0,0);
+
+dl=Lcg/Sections;
+for i=1:Angle_N
+    L=0;
     for j=1:Sections
-        [Sigma_vm_max(j,i)]=Stress_P(F_shear_CG(j,i),F_axial_CG(j,i),M_shear_CG(j,i),0,Section,Ixx,SecArea,X,Y,Tx,Ty);
+        if L==0
+            switch_hole=1;
+            D=50/2;
+        elseif abs(L-Lck)<=Lcg/(Sections-20)
+            switch_hole=1;
+            D=50/2;
+        else
+            D=0;
+            switch_hole=0;
+        end
+        [Sigma_vm_max(j,i)]=Stress_P(F_shear_CG(j,i),F_axial_CG(j,i),M_shear_CG(j,i),0,Section,Ixx,SecArea,X,Y,Tx,Ty,switch_hole,D);
+        L=L+dl;
     end
 end
 
@@ -129,8 +146,60 @@ A = max(Sigma_vm_max, [], 'all');
 figure(5)
 surf(theta,l_array,Sigma_vm_max);
 
+%%
+%Axial
+for i=1:Sections
+        F_axial_AC(i,:)=Fcx.*sind(theta_2)-Fcy.*cosd(theta_2);
+end
+
+%Shear
+for i=1:Sections
+        F_shear_AC(i,:)=Fcy.*sind(theta_2)+Fcx.*cosd(theta_2);
+end
+
+%Moment
+dL=Lad/Sections;
+l=dL;
+for i=1:Sections
+    M_shear_AC(i,:)=Fcy.*cosd(theta_2)*l+Fcx.*sind(theta_2)*l;
+    l=l+dL;
+end
+%Pure Moment
+L_diag_base=sqrt(Lcdx^2+Lcdy^2);
+theta_temp=acosd(Lcdy/L_diag_base);
+temp_angle=90-theta_temp+theta_2;
+for i=1:Sections
+    M_AC(i,:)=-Fcy*L_diag_base.*cosd(temp_angle)+Fcx*L_diag_base.*sind(temp_angle);
+end
+
+F_shear_AC=flip(F_shear_AC);
+M_shear_AC=flip(M_shear_AC);
+F_axial_AC=flip(F_axial_AC);
+M_AC=flip(M_AC);
+
+D=0;%% Don't change
+switch_hole=0;%% Don't change
+
+for k=1:83
+    [Designation_temp,Section,Ixx,SecArea,X,Y,Tx,Ty]=Data(Designation,1,k);
+    for i=1:Angle_N
+        for j=1:Sections
+            [Sigma_vm_max(j,i)]=Stress_P(F_shear_AC(j,i),F_axial_AC(j,i),M_shear_AC(j,i),M_AC(j,i),Section,Ixx,SecArea,X,Y,Tx,Ty,switch_hole,D);
+        end
+    end
+    A = max(Sigma_vm_max, [], 'all');
+    if A<=Expected_Strength
+        X=[Designation_temp,A];
+        disp(X);
+    end
+end
+
+
+
+  
+
 %% 
-function [Section,Ixx,SecArea,X,Y,Tx,Ty] = Data(Designation)
+function [Designation,Section,Ixx,SecArea,X,Y,Tx,Ty] = Data(Designation,Temp_switch,loop)
 opts = spreadsheetImportOptions("NumVariables", 12);
 
 % Specify sheet and range
@@ -150,21 +219,32 @@ Crosssectionproperties = readtable("C:\Users\ripun\MTech_Project\data\Cross sect
 l=height(Crosssectionproperties);
 clear opts
 l=height(Crosssectionproperties);
-for loop=1:l
-    if Designation==Crosssectionproperties.Designation(loop)
-        Section=Crosssectionproperties.Section(loop);
-        Ixx=Crosssectionproperties.Ix(loop);
-        SecArea=Crosssectionproperties.A(loop);
-        X=Crosssectionproperties.B(loop);
-        Y=Crosssectionproperties.D(loop);
-        Tx=Crosssectionproperties.t(loop);
-        Ty=Crosssectionproperties.T(loop);
+if Temp_switch==0
+    for loop=1:l
+        if Designation==Crosssectionproperties.Designation(loop)
+            Section=Crosssectionproperties.Section(loop);
+            Ixx=Crosssectionproperties.Ix(loop);
+            SecArea=Crosssectionproperties.A(loop);
+            X=Crosssectionproperties.B(loop);
+            Y=Crosssectionproperties.D(loop);
+            Tx=Crosssectionproperties.t(loop);
+            Ty=Crosssectionproperties.T(loop);
+        end
     end
+else
+         Designation=Crosssectionproperties.Designation(loop+140);
+         Section=Crosssectionproperties.Section(loop+140);
+         Ixx=Crosssectionproperties.Ix(loop+140);
+         SecArea=Crosssectionproperties.A(loop+140);
+         X=Crosssectionproperties.B(loop+140);
+         Y=Crosssectionproperties.D(loop+140);
+         Tx=Crosssectionproperties.t(loop+140);
+         Ty=Crosssectionproperties.T(loop+140);
 end
 
 end
 %% 
-function [Sigma_vm_max] = Stress_P(F_shear_CG,F_a,M_shear_CG,M,Section,Ixx,SecArea,X,Y,Tx,Ty)
+function [Sigma_vm_max] = Stress_P(F_shear_CG,F_a,M_shear_CG,M,Section,Ixx,SecArea,X,Y,Tx,Ty,switch_hole,D)
         Sigma_min = 0;
         Sigma_min_2 = 0;
 %         [Section,Ixx,SecArea,X,Y,Tx,Ty]=Data(Designation);
@@ -187,9 +267,18 @@ switch Section
         Sigma_min_In_2 = 0;
 
     case 'B'
-        
-        SecArea_1=SecArea*10^-4;
-        Ixx_1=Ixx*10^-8;
+        if switch_hole==1
+            SecArea_1=SecArea-2*Tx*2*D/100;
+            Ixx_1=Ixx-2*Tx*D^3/12;
+            F_a=2.8*F_a;
+            F_shear_CG=2.8*F_shear_CG;
+            M_shear_CG=M_shear_CG*2*Y/D;
+        else
+            SecArea_1=SecArea;
+            Ixx_1=Ixx;
+        end
+        SecArea_1=SecArea_1*10^-4;
+        Ixx_1=Ixx_1*10^-8;
         Sigma_T = F_a/SecArea_1;
         Sigma_M = M_shear_CG*(Y/2)/Ixx_1*10^(-3);
         Sigma_MM = M*(Y/2)/Ixx_1*10^(-3);
@@ -205,6 +294,7 @@ switch Section
         Sigma_min_In = (Sigma_M_temp/2-sqrt((Sigma_M_temp/2)^2+tau^2))/10^6;
         Sigma_max_In_2 = (Sigma_M_temp_2/2+sqrt((Sigma_M_temp_2/2)^2+tau^2))/10^6;
         Sigma_min_In_2 = (Sigma_M_temp_2/2-sqrt((Sigma_M_temp_2/2)^2+tau^2))/10^6;
+        
     case 'L'
         
         SecArea_1=SecArea*10^-4;
